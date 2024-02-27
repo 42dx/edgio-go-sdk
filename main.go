a5e646773e7d7d9e38e15462d10bf3b1b954d615
Rafael Eduardo Paulin <rafapaulin@gmail.com>
2024-02-26 21:05:07 -0300

    chore: main.go to test sdk

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	"42dx/edgio"
+	"42dx/pkg/client"
+	"42dx/pkg/common"
+	"42dx/pkg/org"
+	"fmt"
 	"os"
 )
 
 func main() {
-    creds := edgio.EdgioCreds{
+    credentials := common.Creds{
         Key: os.Getenv("EDGIO_CLIENT_KEY"),
         Secret: os.Getenv("EDGIO_CLIENT_SECRET"),
     }
 
-    edgio.SetCreds(creds)
-    edgio.GetAccessToken()
+    orgClient := org.NewClient(org.ClientParams{
+        Credentials: credentials,
+        Config: client.EdgioClientConfig{ OrgId: os.Getenv("ORG_ID") },
+    })
+
+    fmt.Println(orgClient.Get(client.UrlParams{}))
 }
