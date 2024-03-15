73f669da3b587b9f972f7bc35b3ee08e5dc4b9f8
Rafael Eduardo Paulin <rafapaulin@gmail.com>
2024-03-15 14:37:02 -0300

    ref(env-var): changed List due to GetHTTPJSONResult changes [#40]

--- variable/list.go
+++ variable/list.go
@@ -6,27 +6,29 @@ import (
 	"errors"
 	"net/http"
 	"net/url"
+
+	"github.com/mitchellh/mapstructure"
 )
 
-type ListResultType struct {
+type listResultType struct {
 	common.BaseListResultType
-	Items []Variavle `json:"items"`
+	Items []Variable `json:"items"`
 }
 
-var VariableListResult = ListResultType{}
+var listResult = listResultType{}
 
 // List Lists the environment variables for a given Environment.
 // Edgio's list page size was defaulted to 100 for now,
 // which is the highest value. The idea is to return all environment variables
 // until actual pagination is implemented.
 // Returns a list of environment variables for a given Environment, or an error if anything goes wrong.
-func (c ClientStruct) List(environmentID string) (ListResultType, error) {
+func (c ClientStruct) List(environmentID string) (listResultType, error) {
 	httpClient := &http.Client{}
 	serviceURL := c.GetServiceURL(common.URLParams{})
 
 	parsedURL, err := url.Parse(serviceURL)
 	if err != nil {
-		return ListResultType{}, errors.New(err.Error())
+		return listResultType{}, errors.New(err.Error())
 	}
 
 	rawQueryString := utils.ToQueryString(
@@ -37,13 +39,15 @@ func (c ClientStruct) List(environmentID string) (ListResultType, error) {
 
 	request, err := http.NewRequest(http.MethodGet, parsedURL.String(), nil)
 	if err != nil {
-		return ListResultType{}, errors.New(err.Error())
+		return listResultType{}, errors.New(err.Error())
 	}
 
-	err = utils.GetHTTPJSONResult(httpClient, request, c.AccessToken, &VariableListResult)
+	variables, err := utils.GetHTTPJSONResult(httpClient, request, c.AccessToken)
 	if err != nil {
-		return ListResultType{}, errors.New(err.Error())
+		return listResultType{}, errors.New(err.Error())
 	}
 
-	return VariableListResult, nil
+	mapstructure.Decode(variables, &listResult)
+
+	return listResult, nil
 }
